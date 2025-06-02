319e63b12af875f3cb9639bfe9ee17ec1969a120
Oleksandr Pyliavets <Oleksandr Pyliavets@DESKTOP-7JRVHFS>
2025-06-02 11:36:28 +0300

    Added HW 6

---
+++ HW 6/6.1/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _6._1
+{
+    public enum Genres
+    {
+        Drama = 1,
+        Comedy,
+        Tragedy,
+    }
+    public class ThePlay : IDisposable
+    {
+        public bool disposed = false;
+        string? PlayName { get; set; }
+        string? AuthorName { get; set; }
+        Genres PlayGenre { get; set; }
+        int PlayYOB { get; set; }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Managed resources cleaned");
+                }
+                Console.WriteLine("Unmanaged resources cleaned");
+                disposed = true;
+            }
+        }
+        ~ThePlay()
+        {
+            Console.WriteLine("Finalizer called");
+            Dispose(false);
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"|The play {PlayName} was written on {PlayYOB} by {AuthorName}, it represents {PlayGenre} genre|");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                long mem = GC.GetTotalMemory(true);
+                Console.WriteLine($"Memory before clean up: {mem}");
+                ThePlay play = new ThePlay
+                {
+                    PlayName = "Romeo and Juliet",
+                    PlayYOB = 1591,
+                    AuthorName = "William Shakespeare",
+                    PlayGenre = Genres.Tragedy
+                };
+                try
+                {
+                    play.ShowInfo();
+                }
+                finally
+                {
+                    play.Dispose();
+                }
+                long mem1 = GC.GetTotalMemory(true);
+                Console.WriteLine($"Memory after clean up: {mem1}");
+            }
+        }
+    }
+}
---
+++ HW 6/6.2/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _6._2
+{
+    public enum MarketType
+    {
+        Product = 1,
+        Domestic,
+        Clothes,
+        Shoes
+    }
+    public class Address
+    {
+        public string? CityName { get; set; }
+        public string? StreetName { get; set; }
+        public int StreetNum { get; set; }
+        public int ZipCode { get; set; }
+        public override string ToString()
+        {
+            return $"{CityName}, {StreetName} {StreetNum}, {ZipCode}";
+        }
+    }
+    public class Market : IDisposable
+    {
+        public bool disposed = false;
+        public MarketType Type { get; set; }
+        public string? MarkeName { get; set; }
+        public Address Address { get; set; }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Managed resources cleaned");
+                }
+                Console.WriteLine("Unmanaged resources cleaned");
+                disposed = true;
+            }
+        }
+        public void MarketInfo()
+        {
+            Console.WriteLine($"{Type} market {MarkeName} is located at the address: {Address}");
+        }
+        ~Market()
+        {
+            Dispose(false);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Market m = new Market { Type = MarketType.Product, MarkeName = "AAA" };
+            m.Address = new Address { CityName = "B", StreetName = "C", StreetNum = 10, ZipCode = 123 };
+            using (m)
+            {
+                m.MarketInfo();
+            }
+        }
+    }
+}
