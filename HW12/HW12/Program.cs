603ef0bdd50fea167baf3c360d0e5d00b1e9572b
Oleksandr Pyliavets <Oleksandr Pyliavets@DESKTOP-7JRVHFS>
2025-08-05 14:46:00 +0300

    Added HW12 HTML/CSS

---
+++ HW12/HW12/Program.cs
@@ -0,0 +1,15 @@
+namespace HW12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
